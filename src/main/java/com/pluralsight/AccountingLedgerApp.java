640a0b476a422c017d96871dfd2545cc73fca6c9
KyrraRose <krbosland1@gmail.com>
2025-10-10 12:01:46 -0400

    Working on the deposit!

--- src/main/java/com/pluralsight/AccountingLedgerApp.java
+++ src/main/java/com/pluralsight/AccountingLedgerApp.java
@@ -1,5 +1,10 @@
 package com.pluralsight;
 
+import java.io.*;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class AccountingLedgerApp {
@@ -26,10 +31,11 @@ public class AccountingLedgerApp {
     public static void menuSelector(){
         Scanner scanner = new Scanner(System.in);
         System.out.print("Type Here: ");
-        String choice = scanner.nextLine();
+        String choice = scanner.nextLine().trim().toUpperCase();
         switch (choice){
             case "D":
                 //add deposit!
+                addDeposit(scanner);
                 break;
             case "P":
                 //make payment!
@@ -47,5 +53,33 @@ public class AccountingLedgerApp {
         }
         System.out.println("----");
         System.out.println("Press [ENTER] to continue..");
+    }
+    public static ArrayList<Transaction> loadLedger(){
+        ArrayList<Transaction> ledger = new ArrayList<>();
+        try {
+            BufferedReader bufReader = new BufferedReader(new FileReader("src/main/resources/transactions.csv"));
+            String input;
+            while((input = bufReader.readLine()) != null) {
+                String[] tokens = input.split("\\|");
+                DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+                LocalDate date = LocalDate.parse(tokens[0],dateFormat);
+                LocalTime time = ;//Lunchtime notes format time!!
+
+
+
+
+                ledger.add(new Transaction(date,time,description,vendor,amount));
+            }
+
+            bufReader.close();
+        } catch (IOException e) {
+            e.printStackTrace(); //Error chain
+        }
+
+    }
+    public static void addDeposit(Scanner scanner){
+
+
     }
 }
