fde7db7dcc7a227b72e7bb3a1fd9a8acb80627ff
KyrraRose <krbosland1@gmail.com>
2025-10-10 11:26:27 -0400

    Menu Methods made!

--- src/main/java/com/pluralsight/AccountingLedgerApp.java
+++ src/main/java/com/pluralsight/AccountingLedgerApp.java
@@ -1,4 +1,51 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class AccountingLedgerApp {
+    public static void main(String[] args) {
+        //We'll start here:
+        System.out.println("=== Welcome to the Accounting Ledger ===");
+
+        while (true){
+            mainMenu();
+            menuSelector();
+        }
+
+    }
+    public static void mainMenu(){
+        System.out.println();
+        System.out.println("     ==[Main Menu]==");
+        System.out.println("What would you like to do?");
+        System.out.println("  [D] Add Deposit");
+        System.out.println("  [P] Make Payment (Debit)");
+        System.out.println("  [L] Display Ledger");
+        System.out.println("  [X] Exit the Application");
+    }
+
+    public static void menuSelector(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Type Here: ");
+        String choice = scanner.nextLine();
+        switch (choice){
+            case "D":
+                //add deposit!
+                break;
+            case "P":
+                //make payment!
+                break;
+            case "L":
+                //display ledger!
+                break;
+            case "X":
+                System.out.println("Exiting - Have a Nice Day!");
+                scanner.close();
+                System.exit(0);
+                break;
+            default:
+                System.out.println("Input not recognized, try again!");
+        }
+        System.out.println("----");
+        System.out.println("Press [ENTER] to continue..");
+    }
 }
